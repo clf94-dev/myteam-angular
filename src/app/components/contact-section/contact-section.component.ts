a97b9f5c9984a6097dac659e08fcbabb40f58eb7
clf94-dev <clf94@hotmail.es>
2020-10-30 08:31:40 +0100

    added contactSection component

--- src/app/components/contact-section/contact-section.component.ts
+++ src/app/components/contact-section/contact-section.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FeaturesData } from '../features-home/featuresData';
 
 @Component({
   selector: 'app-contact-section',
@@ -6,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-section.component.scss']
 })
 export class ContactSectionComponent implements OnInit {
-
-  constructor() { }
+  public features : FeaturesData[];
+  constructor() { 
+    this.features = [
+      new FeaturesData('', 'The quality of our talent network',
+      'Experienced Individuals', '../../../assets/images/icon-person.svg'),
+      new FeaturesData('', 'Usage & implementation of our software',
+      'Easy to Implement', '../../../assets/images/icon-cog.svg'),
+      new FeaturesData('', 'How we help drive innovation',
+      'Enhanced Productivity', '../../../assets/images/icon-chart.svg')
+  ];
+  }
 
   ngOnInit(): void {
   }
