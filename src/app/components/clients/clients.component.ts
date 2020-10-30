c3b7941684efaa53e75881880eddf50aa5769111
clf94-dev <clf94@hotmail.es>
2020-10-30 08:15:52 +0100

    added  companies component

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clients.component.scss']
 })
 export class ClientsComponent implements OnInit {
-
+  Gadgets='../../../assets/images/logo-gadgets-now.png';
+  Jakarta='../../../assets/images/logo-jakarta-post.png';
+  Tech='../../../assets/images/logo-tech-radar.png';
+  TheGuardian='../../../assets/images/logo-the-guardian.png';
+  Verge='../../../assets/images/logo-the-verge.png';
   constructor() { }
 
   ngOnInit(): void {
